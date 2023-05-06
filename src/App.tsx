1ce53fe0fb3c50d440130a537f2866d29abb24ae
Takayuki Watanabe <nabe@lab.twcu.ac.jp>
2023-05-06 13:15:48 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { UserCard } from "./components/UserCard";
+import "./styles.css";
+import { useAllUsers } from "./hooks/useAllUsers";
+
+export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => getUsers();
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>エラー：データ取得失敗</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
